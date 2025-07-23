7a2fbf7133ecdb0e0a378bd253bd7687e8450a89
EspinosaJV <152014605+EspinosaJV@users.noreply.github.com>
2025-07-23 14:48:30 +0800

    For Today Dashboard Journal Button JS Functionality Implementation
    Implemented an addEventListener to the Journal Button in the For Today Dashboard Button
    
    - JV

---
+++ src/js/pages/fortoday.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const forTodayDashboardNewJournalBtn = document.getElementById("forTodayDashboardNewJournalBtn");
+
+    forTodayDashboardNewJournalBtn.addEventListener("click", (event) => {
+        console.log("New Journal Button has been clicked!");
+    })
+})
